a077528e2941b5c0dd6cc13e60d20f1edd379c80
santi0995 <santiserrato0910@gmail.com>
2023-01-22 22:06:05 +0100

    taskList

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-import react, {useState} from 'react';
+import { Button, FlatList, Modal, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
   
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedTask, setSelectedTask] = useState(null);
 
   const onHandlerChange = (text) => {
     setTask(text)
@@ -20,7 +22,28 @@ export default function App() {
     setTask('');
   }
 
-  console.warn('tasks', tasks)
+  const onHandlerModal = (item) => {
+    setIsModalVisible(!isModalVisible)
+    setSelectedTask(item);
+  }
+
+  const renderItem = ({item}) => (
+    <TouchableOpacity style={styles.itemContainer} onPress={() => onHandlerModal(item)}>
+      <Text style={styles.itemList} >{item.value}</Text>
+    </TouchableOpacity>
+    )
+
+  const keyExtractor = (item) => item.id;
+
+  const onHandlerCancel = () => {
+    setIsModalVisible(!isModalVisible);
+    setSelectedTask(null);
+  }
+
+  const onHandlerDelete = () => {
+    setTasks((prevTaskList) => prevTaskList.filter((task) => task.id != selectedTask.id));
+    setIsModalVisible(!isModalVisible);
+  }
 
   return (
     <View style={styles.container}>
@@ -35,15 +58,32 @@ export default function App() {
         onChangeText={onHandlerChange}/>
         <Button disabled={!task} title='Add' color='#f18f01' onPress={onHandlerSubmit}/>
         </View>
-        <View style={styles.listContainer}>
-          {
-            tasks.map((item) => (
-              <View style={styles.itemContainer}>
-              <Text style={styles.itemList} key={item.id}>{item.value}</Text>
-              </View>
-            ))
-          }
-        </View>
+        <FlatList 
+        data={tasks}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        style={styles.listContainer}
+        />
+        <Modal visible={isModalVisible} animationType='slide'>
+          <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>Si se muestra</Text>
+          <View style={styles.modalDetailContainer}>
+          <Text style={styles.modalDetailMessage}>Quieres eliminar la tarea?</Text>
+          <Text style={styles.selectedTask}>{selectedTask?.value}</Text>
+          </View>
+          <View style={styles.modalButtonContainer}>
+            <Button
+            title='Cancelar'
+            color='#f18f01'
+            onPress={onHandlerCancel}
+            />
+            <Button
+            title= 'Eliminar'
+            color= 'red'
+            onPress={onHandlerDelete}/>
+          </View>
+          </View>
+        </Modal>
     </View>
   );
 }
@@ -80,9 +120,41 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     borderRadius: 10,
   },
-  itemlist:{
+  itemList:{
     fontSize: 14,
-    color: '#fff',
+    color: '#ffffff',
     fontWeight: 'bold'
+  },
+  modalContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 50,
+    paddingVertical: 20,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  modalDetailContainer: {
+    paddingVertical: 20,
+  },
+  modalDetailMessage: {
+    fontSize: 14,
+    color: '#f18f01'
+  },
+  selectedTask: {
+    fontSize: 14,
+    color: '#f18d01',
+    fontWeight: 'bold',
+    paddingVertical: 10,
+    textAlign: 'center',
+    marginBottom: 20
+  },
+  modalButtonContainer: {
+    width: '70%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginHorizontal: 20,
   },
 });
